23c22702438949c481a2ef70200ce6f27d6b92b0
xd <dsawyer@tutanota.com>
2019-06-15 13:44:34 -0400

    combined all components into ResultsPage and LandingPage

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,46 +1,47 @@
 import React from 'react'
-import LandingNavBar from './LandingNavBar'
-import {
-  Form,
-  Col,
-  Button
-} from 'react-bootstrap'
+import { Form, Col, Button, Navbar } from 'react-bootstrap'
 
+const LandingNavBar = () => (
+  <Navbar bg='dark' variant='dark'>
+    <Navbar.Brand href='#home'>
+      <img alt='ConSearch Logo' src='' className='d-inline-block align-top' />
+      {' ConSearch '}
+    </Navbar.Brand>
+  </Navbar>
+)
 
 const SearchForm = () => {
-  return(
-      <Form>
-          <Form.Row>
-              <Col>
-                  <Form.Control placeholder="Enter your search..." />
-              </Col>
-              <Col>
-                  <Form.Label>Distance</Form.Label>
-                  <Form.Control as="select">
-                      <option>Choose...</option>
-                      <option>10 mi</option>
-                      <option>25 mi</option>
-                      <option>50 mi</option>
-                      <option>75 mi</option>
-                      <option>100 mi</option>
-                  </Form.Control>
-              </Col>
-              <Col>
-                  <Button variant="primary">
-                      Search
-                  </Button>
-              </Col>
-          </Form.Row>
-      </Form>
+  return (
+    <Form>
+      <Form.Row>
+        <Col>
+          <Form.Control placeholder='Enter your search...' />
+        </Col>
+        <Col>
+          <Form.Label>Distance</Form.Label>
+          <Form.Control as='select'>
+            <option>Choose...</option>
+            <option>10 mi</option>
+            <option>25 mi</option>
+            <option>50 mi</option>
+            <option>75 mi</option>
+            <option>100 mi</option>
+          </Form.Control>
+        </Col>
+        <Col>
+          <Button variant='primary'>Search</Button>
+        </Col>
+      </Form.Row>
+    </Form>
   )
 }
 
-const LandingPage = () => 
+const LandingPage = () => (
   <>
     <LandingNavBar />
     <div className='landing-info'>Find local music anywhere you go.</div>
     <SearchForm />
   </>
-
+)
 
 export default LandingPage
--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -1,8 +1,47 @@
-import React, { useEffect }  from 'react'
-import NavBar from './NavBar'
+import React, { useEffect } from 'react'
 // import Result from './Result'
 import { useSelector, useDispatch } from 'react-redux'
-import {FETCH_RESULTS} from '../actions/index'
+import { FETCH_RESULTS } from '../actions/index'
+import { Form, Button, Navbar, FormControl } from 'react-bootstrap'
+
+const NavBar = () => (
+  <Navbar bg='dark' variant='dark'>
+    <NavBar.Brand href='#home'>
+      <img alt='ConSearch Logo' src='' className='d-inline-block align-top' />
+      {' ConSearch '}
+    </NavBar.Brand>
+    <Form inline>
+      <FormControl
+        type='text'
+        placeholder='Search by city...'
+        className='mr-sm-2'
+      />
+      <Button variant='outline-info'>Search</Button>
+    </Form>
+  </Navbar>
+)
+
+const Result = props => (
+  <div className='container'>
+    <img src={props.img} alt='artist'/>
+    <div className='content'>
+      <div className='info'>
+        <div className='date'>{props.date}</div>
+        <div className='artist'>{props.artist}</div>
+        <div className='venue'>{props.venue}</div>
+      </div>
+      <div className='buttons'>
+        <Button variant='outline-primary'>Play Music</Button>
+        <Button variant='outline-primary'>Buy Tickets</Button>
+      </div>
+      <div className='audio-player'>
+        <audio controls>
+          <source src={props.song} type='audio/mpeg' />
+        </audio>
+      </div>
+    </div>
+  </div>
+)
 
 const ResultsPage = () => {
   const results = useSelector(state => state.results)
@@ -11,15 +50,14 @@ const ResultsPage = () => {
     dispatch({ type: FETCH_RESULTS })
   })
 
-    // const results = this.props.results.map((result, index) => {}
+  // const results = this.props.results.map((result, index) => {}
 
-    return (
-      <div className='results-page'>
-        <NavBar />
-        {results}
-      </div>
-      
-    )
-  }
+  return (
+    <div className='results-page'>
+      <NavBar />
+      {results}
+    </div>
+  )
+}
 
- export default ResultsPage
+export default ResultsPage
