b180107b5e058ef748c5737650b36d793513a1e7
xd <dsawyer@tutanota.com>
2019-07-08 21:32:48 -0400

    added basic styling/formatting

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,46 +1,44 @@
 import React, { useState, useContext } from 'react'
-import { Form, Col, Button, Navbar } from 'react-bootstrap'
 import { StoreContext } from '../store'
 import { withRouter } from 'react-router-dom'
+import { Form, Button } from 'react-bootstrap'
 import { TM_URL } from '../apiUrl'
+import './styling/css/landing.css'
+import './styling/css/common.css'
 
 const LandingNavBar = () => (
-  <Navbar bg='dark' variant='dark'>
-    <Navbar.Brand href='#home'>
-      <img alt='ConSearch Logo' src='' className='d-inline-block align-top' />
-      {' ConSearch '}
-    </Navbar.Brand>
-  </Navbar>
+  // make a link
+  <div className='logo'>ConSearch</div>
 )
-
 
 export const SearchForm = withRouter(({ history }) => {
   /* When city & distance inputs change, the hooks for city and distance change
-  their state. */ 
+  their state. */
+
   const [city, setCity] = useState('')
   const [distance, setDistance] = useState('')
   const [state, dispatch] = useContext(StoreContext)
 
-  //Fetch results from ticketMaster API and set it in context
+  // Fetch results from ticketMaster API and set it in context
   const fetchResults = (city, distance) => {
     fetch(`${TM_URL}&city=${city}&distance=${distance}`)
       .then(res => res.json())
       .then(response => {
-        //condense these
-        dispatch({type: 'FETCH_RESULTS', payload: response})
-        dispatch({type: 'SET_CITY', payload: city})
-        dispatch({type: 'SET_DISTANCE', payload: distance})
-        dispatch({type: 'SET_PAGE'})
-        dispatch({type: 'SET_LAST_PAGE', payload: response})
-    })
+        // condense these
+        dispatch({ type: 'FETCH_RESULTS', payload: response })
+        dispatch({ type: 'SET_CITY', payload: city })
+        dispatch({ type: 'SET_DISTANCE', payload: distance })
+        dispatch({ type: 'SET_PAGE' })
+        dispatch({ type: 'SET_LAST_PAGE', payload: response })
+      })
       .catch(err => console.error('ERROR:', err.message))
   }
 
-  /*on submit of the form, the city and distance state are sent as query
-  parameters for the fetchResults function, redirect to results page*/
+  /* on submit of the form, the city and distance state are sent as query
+  parameters for the fetchResults function, redirect to results page */
   const onSubmit = e => {
     e.preventDefault()
-    if (city.trim() === '') return;
+    if (city.trim() === '') return
     let distNum = distance.substr(0, distance.length - 3)
     fetchResults(city, distNum)
     setCity('')
@@ -49,46 +47,48 @@ export const SearchForm = withRouter(({ history }) => {
   }
 
   return (
-    <Form onSubmit={onSubmit}>
-      <Form.Row>
-        <Col>
-          <Form.Control
-            placeholder='Enter your search...'
-            onChange={e => setCity(e.target.value)}
-            value={city}
-            name='city'
-          />
-        </Col>
-        <Col>
-          <Form.Label>Distance</Form.Label>
-          <Form.Control
-            as='select'
-            onChange={e => setDistance(e.target.value)}
-            value={distance}
-            name='distance'
-          >
-            <option>Choose...</option>
-            <option>10 mi</option>
-            <option>25 mi</option>
-            <option>50 mi</option>
-            <option>75 mi</option>
-            <option>100 mi</option>
-          </Form.Control>
-        </Col>
-        <Col>
-          <Button type='submit' variant='primary'>Search</Button>
-        </Col>
-      </Form.Row>
+    <Form className='search-form' onSubmit={onSubmit}>
+      <Form.Control
+        className='city-input input'
+        type='text'
+        placeholder='Enter a City...'
+        onChange={e => setCity(e.target.value)}
+        value={city}
+        name='city'
+      />
+      <Form.Control
+        className='dist-input input'
+        as='select'
+        onChange={e => setDistance(e.target.value)}
+        value={distance}
+        name='distance'
+        placeholder='Distance...'
+      >
+        <option>10 mi</option>
+        <option>25 mi</option>
+        <option>50 mi</option>
+        <option>75 mi</option>
+        <option>100 mi</option>
+      </Form.Control>
+
+      <Button type='submit'>Search</Button>
     </Form>
   )
 })
 
 const LandingPage = () => (
-  <>
+  <body className='landing-body'>
     <LandingNavBar />
-    <div className='landing-info'>Find local music anywhere you go.</div>
+    <div className='landing-content'>
+      <h1>Find Local Music</h1>
+      <h1>Wherever You Are</h1>
+      <p>
+        ConSearch lets you search for upcoming music events and lets you listen
+        to what will be playing at the event.
+      </p>
+    </div>
     <SearchForm />
-  </>
+  </body>
 )
 
 export default LandingPage
--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -1,7 +1,9 @@
 import React, { useState, useContext, useEffect } from 'react'
-import { Form, Col, Button, Navbar } from 'react-bootstrap'
 import { StoreContext } from '../store'
 import { TM_URL, ITUNES_URL } from '../apiUrl'
+import { Form, Button } from 'react-bootstrap'
+import './styling/css/results.css'
+import './styling/css/common.css'
 
 const ResultsBar = () => {
   const [city, setCity] = useState('')
@@ -12,139 +14,150 @@ const ResultsBar = () => {
     fetch(`${TM_URL}&city=${city}&distance=${distance}`)
       .then(res => res.json())
       .then(response => {
-        dispatch({type: 'FETCH_RESULTS', payload: response})
-        dispatch({type: 'SET_CITY', payload: city})
-        dispatch({type: 'SET_DISTANCE', payload: distance})
-        dispatch({type: 'SET_PAGE'})
+        dispatch({ type: 'FETCH_RESULTS', payload: response })
+        dispatch({ type: 'SET_CITY', payload: city })
+        dispatch({ type: 'SET_DISTANCE', payload: distance })
+        dispatch({ type: 'SET_PAGE' })
       })
       .catch(err => console.error('ERROR:', err))
   }
 
   const onSubmit = e => {
     e.preventDefault()
-    if (city.trim() === '') return;
+    if (city.trim() === '') return
     let distNum = distance.substr(0, distance.length - 3)
     fetchResults(city, distNum)
-    // setCity('')
-    // setDistance('Choose...')
   }
-  
-  return(
-    <Navbar bg='dark' variant='dark'>
-      <Navbar.Brand href='/'>
-        <img alt='ConSearch Logo' src='google.com' className='d-inline-block align-top' />
-        {' ConSearch '}
-      </Navbar.Brand>
-      <Form onSubmit={onSubmit}>
-      <Form.Row>
-        <Col>
-          <Form.Control
-            placeholder='Enter your search...'
-            onChange={e => setCity(e.target.value)}
-            value={city}
-            name='city'
-          />
-        </Col>
-        <Col>
-          <Form.Label>Distance</Form.Label>
-          <Form.Control
-            as='select'
-            onChange={e => setDistance(e.target.value)}
-            value={distance}
-            name='distance'
-          >
-            <option>Choose...</option>
-            <option>10 mi</option>
-            <option>25 mi</option>
-            <option>50 mi</option>
-            <option>75 mi</option>
-            <option>100 mi</option>
-          </Form.Control>
-        </Col>
-        <Col>
-          <Button type='submit' variant='primary'>Search</Button>
-        </Col>
-      </Form.Row>
-    </Form>
-    </Navbar>
+
+  return (
+    <nav>
+      <div className='logo nav-logo'>ConSearch</div>
+      <Form className='nav-form' onSubmit={onSubmit}>
+        <Form.Control
+         className='city-input input'
+          placeholder='Enter a city...'
+          onChange={e => setCity(e.target.value)}
+          value={city}
+          name='city'
+        />
+
+        <Form.Control
+          className='dist-input input'
+          as='select'
+          onChange={e => setDistance(e.target.value)}
+          value={distance}
+          name='distance'
+          placeholder='Distance...'
+        >
+          <option>10 mi</option>
+          <option>25 mi</option>
+          <option>50 mi</option>
+          <option>75 mi</option>
+          <option>100 mi</option>
+        </Form.Control>
+        <Button type='submit'>Search</Button>
+      </Form>
+    </nav>
   )
 }
-
-
 
 const Result = props => {
   const [audio, setAudio] = useState('')
   const [error, setError] = useState('')
   const hdPic = props.pics.filter(pic => pic.ratio === '4_3')[0].url
-  const name = (props.rawName.attractions) ? props.rawName.attractions[0].name : ''
+  const name = props.rawName.attractions
+    ? props.rawName.attractions[0].name
+    : ''
 
   const fetchAudio = artist => {
-      const artistURI = encodeURIComponent(artist);
-      fetch(`${ITUNES_URL}${artistURI}`)
-        .then(res => res.json())
-        .then(response => {
-          if (response.results.length < 1) {
-            throw new Error('Invalid Artist')
-          }
-          setError('')
-          setAudio(
-            <audio controls='controls' src={response.results[0].previewUrl}></audio>
-          )
-        })
-        .catch(setError('Music Unavailable'))
-    }
-  
+    const artistURI = encodeURIComponent(artist)
+    fetch(`${ITUNES_URL}${artistURI}`)
+      .then(res => res.json())
+      .then(response => {
+        if (response.results.length < 1) {
+          throw new Error('Invalid Artist')
+        }
+        setError('')
+        setAudio(
+          <audio controls='controls' src={response.results[0].previewUrl} />
+        )
+      })
+      .catch(err => setError('Music Unavailable'))
+  }
+
   return (
-  <div className='container'>
-    <img src={hdPic} alt='artist' />
-    <div className='content'>
-      <div className='info'>
-        <div className='date'>{props.date}</div>
-        <div className='artist'>{props.title}</div>
-        <div className='venue'>@ {props.venue}</div>
+    <div className='result-item'>
+      <div className='photo'>
+       <img src={hdPic} alt='artist' />
       </div>
-      <div className='buttons'>
-        <Button variant='outline-primary' onClick={() => {fetchAudio(name)}}>Play Music</Button>
-        <Button href={props.tmLink} variant='outline-primary'>Buy Tickets</Button>
-      </div> 
-      <div className='audio-player'>
-        {audio}
+      <div className='content'>
+        <div className='info'>
+          <div className='date'>{props.date}</div>
+          <div className='artist'>{props.title}</div>
+          <div className='venue'>@ {props.venue}</div>
+        </div>
+        <div className='buttons'>
+          <Button
+            className='play-button'
+            variant='warning'
+            onClick={() => {
+              fetchAudio(name)
+            }}
+          >
+            Play
+          </Button>
+          <Button className='ticket-button' variant='danger' href={props.tmLink}>Tickets</Button>
+        </div>
+        <div className='audio-player'>{audio}</div>
+        <div className='error'>{error}</div>
       </div>
-      <div className='error'>{error}</div>
     </div>
-  </div>
   )
 }
 
 const Pagination = () => {
   const [state, dispatch] = useContext(StoreContext)
   const currentPage = state.page
-  
-  const lastPage = () => {
-    fetch(`${TM_URL}&city=${state.city}&distance=${state.distance}&page=${currentPage - 1}`)
-    .then(res => res.json())
-    .then(response => {
-      dispatch({type: 'FETCH_RESULTS', payload: response})
-      dispatch({type: 'LAST_PAGE', payload: currentPage-1})
-    })
-    .catch(err => console.error('ERROR:', err.message))
-  }
 
-  const nextPage = () => {
-    fetch(`${TM_URL}&city=${state.city}&distance=${state.distance}&page=${currentPage + 1}`)
+  const lastPage = () => {
+    fetch(
+      `${TM_URL}&city=${state.city}&distance=${
+        state.distance
+      }&page=${currentPage - 1}`
+    )
       .then(res => res.json())
       .then(response => {
-        dispatch({type: 'FETCH_RESULTS', payload: response})
-        dispatch({type: 'NEXT_PAGE', payload: currentPage+1})
+        dispatch({ type: 'FETCH_RESULTS', payload: response })
+        dispatch({ type: 'LAST_PAGE', payload: currentPage - 1 })
       })
       .catch(err => console.error('ERROR:', err.message))
   }
 
-  const lastButton = (currentPage === 1) ? '' : <Button variant='outline-primary' onClick={lastPage}>Last Page</Button>
-  const nextButton = (currentPage <= state.lastPage) ? <Button variant='outline-primary' onClick={nextPage}>NextPage</Button> : ''
+  const nextPage = () => {
+    fetch(
+      `${TM_URL}&city=${state.city}&distance=${
+        state.distance
+      }&page=${currentPage + 1}`
+    )
+      .then(res => res.json())
+      .then(response => {
+        dispatch({ type: 'FETCH_RESULTS', payload: response })
+        dispatch({ type: 'NEXT_PAGE', payload: currentPage + 1 })
+      })
+      .catch(err => console.error('ERROR:', err.message))
+  }
+
+  const lastButton =
+    currentPage === 1 ? '' : <button onClick={lastPage}>Last Page</button>
+  const nextButton =
+    currentPage <= state.lastPage ? (
+      <button onClick={nextPage}>NextPage</button>
+    ) : (
+      ''
+    )
 
   return (
-    <div className="button-container">
+    <div className='button-container'>
       {lastButton}
       {nextButton}
     </div>
@@ -155,25 +168,27 @@ const ResultsPage = () => {
   const [state, dispatch] = useContext(StoreContext)
 
   useEffect(() => {
-    window.scrollTo(0,0)
+    window.scrollTo(0, 0)
   })
-  
+
   return (
-    <div className='results-page'>
-      <ResultsBar />
-      {state.results.map((result, index) => 
-        <Result 
-          rawName={result._embedded}
-          key={index}
-          pics={result.images}
-          date={result.dates.start.localDate}
-          title={result.name}
-          venue={result._embedded.venues[0].name}
-          tmLink={result.url}
-        />
-      )}
-      <Pagination />
-    </div>
+    <body className='results-body'>
+        <ResultsBar />
+          <section className='results'>
+          {state.results.map((result, index) => (
+            <Result
+              rawName={result._embedded}
+              key={index}
+              pics={result.images}
+              date={result.dates.start.localDate}
+              title={result.name}
+              venue={result._embedded.venues[0].name}
+              tmLink={result.url}
+            />
+            ))}
+            <Pagination />
+            </section>
+      </body>
   )
 }
 
